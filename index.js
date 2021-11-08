fd6e7a494802c5bca8801d5a925349989c954240
tasbiha11 <tasbihahussain@yahoo.com>
2021-11-09 01:01:01 +0600

    added port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { query } = require('express');
 require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //middelware
 app.use(cors());
